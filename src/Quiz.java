4ca3f3a6ca2027491ebdf72ec2cf9b14689c9ead
342927951 <342927951@C-G5JWJB2.tdsb.on.ca>
2025-12-10 11:43:08 -0500

    Did some code in the Quiz parent class. T.Y.

---
+++ src/Quiz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 342927951
+ */
+public class Quiz {
+    private int score;
+    private int questionNumber;
+    private Difficulty difficulty;
+    
+    public Quiz(int score, int questionNumber, Difficulty difficulty){
+        this.score = score;
+        this.questionNumber = questionNumber;
+        this.difficulty = difficulty;
+    }
+}
